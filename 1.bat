7b3e847e2f39483747c60f67cad51c3d183fc51c
hwsknudsen <hwsknudsen@users.noreply.github.com>
2020-02-13 11:35:19 -0400

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -6,9 +6,9 @@ cd "C:\PDF"
 "C:\Program Files\gs\gs9.50\bin\gswin64c.exe" -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile="C:\PDF\%test%2.pdf" "C:\PDFSCRIPT\watermark2.ps" "C:\PDF\%test%.pdf"
 "C:\Program Files\gs\gs9.50\bin\gswin64c.exe" -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile="C:\PDF\%test%3.pdf" "C:\PDFSCRIPT\watermark3.ps" "C:\PDF\%test%.pdf"
 
-"C:\Program Files\SumatraPDF\SumatraPDF.exe" "C:\PDF\%test%1.pdf" -print-to "Front"
-"C:\Program Files\SumatraPDF\SumatraPDF.exe" "C:\PDF\%test%2.pdf" -print-to "Front"
-"C:\Program Files\SumatraPDF\SumatraPDF.exe" "C:\PDF\%test%3.pdf" -print-to "Front"
+"C:\PDFSCRIPT\SumatraPDF.exe" "C:\PDF\%test%1.pdf" -print-to "Front"
+"C:\PDFSCRIPT\SumatraPDF.exe" "C:\PDF\%test%2.pdf" -print-to "Front"
+"C:\PDFSCRIPT\SumatraPDF.exe" "C:\PDF\%test%3.pdf" -print-to "Front"
 
 del %test%.pdf
 del %test%1.pdf
